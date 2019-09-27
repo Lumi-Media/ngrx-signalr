26318bad6ce723a54261c043f5fa151159072614
Nivrith <nivrithgomatam@gmail.com>
2019-09-27 10:37:41 +0930

    revert naming to match library conventions

--- src/actions.ts
+++ src/actions.ts
@@ -2,64 +2,64 @@ import 'signalr';
 import { createAction, props, union } from '@ngrx/store';
 
 export const createSignalRHub = createAction(
-    '[NGRX SignalR]  createHub',
+    '@ngrx/signalr/createHub',
     props<{ hubName: string, url?: string | undefined }>()
 );
 
-export const SIGNALR_HUB_UNSTARTED = '[NGRX SignalR]  hubCreated';
+export const SIGNALR_HUB_UNSTARTED = '@ngrx/signalr/hubCreated';
 export const signalrHubUnstarted = createAction(
     SIGNALR_HUB_UNSTARTED,
     props<{ hubName: string, url?: string | undefined }>()
 );
 
 export const startSignalRHub = createAction(
-    '[NGRX SignalR]  startHub',
+    '@ngrx/signalr/startHub',
     props<{ hubName: string, url?: string | undefined, options?: SignalR.ConnectionOptions | undefined,  beforeConnectionStart?: (connection : SignalR.Hub.Connection | undefined) => void }>()
 );
 
 export const stopSignalRHub = createAction(
-    '[NGRX SignalR]  stopHub',
+    '@ngrx/signalr/stopHub',
     props<{hubName: string, url?: string, async?: boolean, notifyServer?: boolean}>()
 )
 
-export const SIGNALR_HUB_FAILED_TO_START = '[NGRX SignalR]  hubFailedToStart';
+export const SIGNALR_HUB_FAILED_TO_START = '@ngrx/signalr/hubFailedToStart';
 export const signalrHubFailedToStart = createAction(
     SIGNALR_HUB_FAILED_TO_START,
     props<{ hubName: string, url?: string | undefined, error: any }>()
 );
 
-export const SIGNALR_CONNECTING = '[NGRX SignalR]  connecting';
+export const SIGNALR_CONNECTING = '@ngrx/signalr/connecting';
 export const signalrConnecting = createAction(
     SIGNALR_CONNECTING,
     props<{ hubName: string, url?: string | undefined }>()
 );
 
-export const SIGNALR_CONNECTED = '[NGRX SignalR]  connected';
+export const SIGNALR_CONNECTED = '@ngrx/signalr/connected';
 export const signalrConnected = createAction(
     SIGNALR_CONNECTED,
     props<{ hubName: string, url?: string | undefined }>()
 );
 
-export const SIGNALR_DISCONNECTED = '[NGRX SignalR]  disconnected';
+export const SIGNALR_DISCONNECTED = '@ngrx/signalr/disconnected';
 export const signalrDisconnected = createAction(
     SIGNALR_DISCONNECTED,
     props<{ hubName: string, url?: string | undefined }>()
 );
 
-export const SIGNALR_RECONNECTING = '[NGRX SignalR]  reconnecting';
+export const SIGNALR_RECONNECTING = '@ngrx/signalr/reconnecting';
 export const signalrReconnecting = createAction(
     SIGNALR_RECONNECTING,
     props<{ hubName: string, url?: string | undefined }>()
 );
 
-export const SIGNALR_ERROR = '[NGRX SignalR]  error';
+export const SIGNALR_ERROR = '@ngrx/signalr/error';
 export const signalrError = createAction(
     SIGNALR_ERROR,
     props<{ hubName: string, url?: string | undefined, error: SignalR.ConnectionError }>()
 );
 
 export const hubNotFound = createAction(
-    '[NGRX SignalR]  hubNotFound',
+    '@ngrx/signalr/hubNotFound',
     props<{ hubName: string, url?: string | undefined }>()
 );
 
